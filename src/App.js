0d6ea5ab08f0880af8490fb397681ccb3df99a6b
Gilwell <gilwellm@gmail.com>
2020-12-31 03:38:14 +0300

    Implementing Routes on Rooms View

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
+import Rooms from './components/Rooms';
+import Header from './components/Shared/Header';
+import Footer from './components/Shared/Footer';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+    <Header />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/rooms" component={Rooms} />
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
-import Header from '../Header';
+import Header from '../Shared/Header';
 import Slider from './Slider';
 import AboutUsHome from './AboutUsHome';
 import DiscoverourServices from '../OurServices/DiscoverourServices'
 import Testimonials from './Testimonials'
 import Blogs from './Blogs';
 import Rooms from '../Rooms'
-import Footer from '../Footer'
-import Search from './Search';
+import Footer from '../Shared/Footer'
+import Search from '../Shared/Search/Search';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <Header />
                 <Slider />
                 <AboutUsHome />
                 <DiscoverourServices />
